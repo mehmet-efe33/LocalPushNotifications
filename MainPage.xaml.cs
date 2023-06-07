194f948bccf4420aef2316f2003b377f5bbe171e
Mehmet Efe Yılmaz <73351132+mehmet-efe33@users.noreply.github.com>
2023-06-07 16:20:06 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Plugin.LocalNotification;
+
+namespace LocalPushNotification;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		var request = new NotificationRequest
+		{
+			NotificationId = 1337,
+			Title = "Hello everyone",
+			Subtitle = "Warning",
+			Description = "It's me",
+			BadgeNumber = 42,
+			Schedule = new NotificationRequestSchedule
+			{
+				NotifyTime = DateTime.Now.AddSeconds(5),
+				NotifyRepeatInterval = TimeSpan.FromDays(1),
+			}
+		};
+
+		LocalNotificationCenter.Current.Show(request);
+	}
+}
+
